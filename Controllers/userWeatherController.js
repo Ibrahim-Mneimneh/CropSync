97c51bc763a77315d9eccd5a56b4c6798edb866d
Ibrahim <ibrahim.mneimneh0@gmail.com>
2024-02-09 15:55:15 +0200

    Fix Daily Weather Functionality

--- Controllers/userWeatherController.js
+++ Controllers/userWeatherController.js
@@ -5,27 +5,26 @@ const User = require("../Models/userModel");
 const getDailyWeather = async (req, res) => {
   try {
     const userId = req.userId;
-    const device = await Device.findOne({ userId });
-    if (!device) {
-      return res.status(404).json({ error: "User not found." });
+    const devices = await Device.find({ userId });
+    if (!devices) {
+      return res.status(404).json({ error: "Please add a device." });
     }
 
-    const location = device.city + ", " + device.country;
-    console.log(location);
-    const endpoint =
-      "http://api.weatherapi.com/v1/current.json?key=" +
-      process.env.weatherKey +
-      "&q=" +
-      location +
-      "&aqi=yes";
-
-    const weatherDataResponse = await fetch(endpoint);
-    if (!weatherDataResponse.ok) {
-      return res.status(400).json({ error: "couldnt" });
-    }
-    const weatherData = await weatherDataResponse.json();
-    console.log(weatherData);
-    return res.status(200).json(weatherData.current);
+    const userWeatherData = await Promise.all(
+      devices.map(async (device) => {
+        const location = device.city + ", " + device.country;
+        const endpoint =
+          "http://api.weatherapi.com/v1/current.json?key=" +
+          process.env.weatherKey +
+          "&q=" +
+          location +
+          "&aqi=yes";
+        const weatherDataResponse = await fetch(endpoint);
+        const weatherData = await weatherDataResponse.json();
+        return { name: device.name, location, ...weatherData.current };
+      })
+    );
+    return res.status(200).json(userWeatherData);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
