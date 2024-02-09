8330c8ff0ecbe673aa9c7614990a0b523595f5a2
Zouheir Nakouzi <61628216+ZouheirN@users.noreply.github.com>
2024-02-09 19:29:43 +0200

    Update server.js
    increased upload limit

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 
 dotenv.config();
 
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use((req, res, next) => {
   console.log(req.path);
